20691015a1385b26466891338513b9f435a4b4fe
Eval Exec <execvy@gmail.com>
2026-02-14 15:19:58 +0800

    vm-compat: add varset bytecode literal execution coverage

--- rust/neovm-core/src/elisp/eval.rs
+++ rust/neovm-core/src/elisp/eval.rs
@@ -2629,7 +2629,8 @@ mod tests {
              (funcall (car (read-from-string \"#[(x y) \\\"\\\\302\\\\10\\\\11\\\\42\\\\207\\\" [x y logxor] 3]\")) 6 3)
              (funcall (car (read-from-string \"#[(x) \\\"\\\\301\\\\10\\\\41\\\\207\\\" [x lognot] 2]\")) 0)
              (funcall (car (read-from-string \"#[(x y) \\\"\\\\302\\\\10\\\\11\\\\42\\\\207\\\" [x y ash] 3]\")) 3 2)
-             (funcall (car (read-from-string \"#[(a b c d e f g h) \\\"\\\\10\\\\11\\\\12\\\\13\\\\14\\\\15\\\\16\\\\6\\\\16\\\\7\\\\257\\\\10\\\\207\\\" [a b c d e f g h] 8]\")) 1 2 3 4 5 6 7 8)",
+             (funcall (car (read-from-string \"#[(a b c d e f g h) \\\"\\\\10\\\\11\\\\12\\\\13\\\\14\\\\15\\\\16\\\\6\\\\16\\\\7\\\\257\\\\10\\\\207\\\" [a b c d e f g h] 8]\")) 1 2 3 4 5 6 7 8)
+             (funcall (car (read-from-string \"#[(a b c d e f g h) \\\"\\\\11\\\\20\\\\10\\\\16\\\\7\\\\26\\\\6\\\\16\\\\6\\\\104\\\\207\\\" [a b c d e f g h] 2]\")) 1 2 3 4 5 6 7 8)",
         );
         assert_eq!(results[0], "OK t");
         assert_eq!(results[1], "OK t");
@@ -2671,6 +2672,7 @@ mod tests {
         assert_eq!(results[37], "OK -1");
         assert_eq!(results[38], "OK 12");
         assert_eq!(results[39], "OK (1 2 3 4 5 6 7 8)");
+        assert_eq!(results[40], "OK (2 8)");
     }
 
     #[test]
