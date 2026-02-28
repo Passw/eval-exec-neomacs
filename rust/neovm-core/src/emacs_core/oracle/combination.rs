3dce91b626197396b80d9231a9551723e8b9c267
Eval Exec <execvy@gmail.com>
2026-03-01 02:21:47 +0800

    oracle: add two-alias cross-advice-remove proptest

--- rust/neovm-core/src/emacs_core/oracle/combination.rs
+++ rust/neovm-core/src/emacs_core/oracle/combination.rs
@@ -4195,6 +4195,58 @@ proptest! {
     }
 
     #[test]
+    fn oracle_prop_combination_two_aliases_cross_advice_remove_consistency(
+        n in -10_000i64..10_000i64,
+    ) {
+        return_if_neovm_enable_oracle_proptest_not_set!(Ok(()));
+
+        let form = format!(
+            "(progn
+               (fset 'neovm--combo-prop-two-alias-target (lambda (x) x))
+               (defalias 'neovm--combo-prop-two-alias-a 'neovm--combo-prop-two-alias-target)
+               (defalias 'neovm--combo-prop-two-alias-b 'neovm--combo-prop-two-alias-target)
+               (fset 'neovm--combo-prop-two-alias-filter (lambda (ret) (+ ret 7)))
+               (unwind-protect
+                   (list
+                     (progn
+                       (advice-add 'neovm--combo-prop-two-alias-a :filter-return 'neovm--combo-prop-two-alias-filter)
+                       (list
+                         (funcall 'neovm--combo-prop-two-alias-a {n})
+                         (funcall 'neovm--combo-prop-two-alias-b {n})
+                         (funcall 'neovm--combo-prop-two-alias-target {n})
+                         (advice-member-p 'neovm--combo-prop-two-alias-filter 'neovm--combo-prop-two-alias-a)
+                         (advice-member-p 'neovm--combo-prop-two-alias-filter 'neovm--combo-prop-two-alias-b)
+                         (advice-member-p 'neovm--combo-prop-two-alias-filter 'neovm--combo-prop-two-alias-target)))
+                     (progn
+                       (advice-remove 'neovm--combo-prop-two-alias-b 'neovm--combo-prop-two-alias-filter)
+                       (list
+                         (funcall 'neovm--combo-prop-two-alias-a {n})
+                         (funcall 'neovm--combo-prop-two-alias-b {n})
+                         (funcall 'neovm--combo-prop-two-alias-target {n})
+                         (advice-member-p 'neovm--combo-prop-two-alias-filter 'neovm--combo-prop-two-alias-a)
+                         (advice-member-p 'neovm--combo-prop-two-alias-filter 'neovm--combo-prop-two-alias-b)
+                         (advice-member-p 'neovm--combo-prop-two-alias-filter 'neovm--combo-prop-two-alias-target))))
+                 (condition-case nil
+                     (advice-remove 'neovm--combo-prop-two-alias-a 'neovm--combo-prop-two-alias-filter)
+                   (error nil))
+                 (condition-case nil
+                     (advice-remove 'neovm--combo-prop-two-alias-b 'neovm--combo-prop-two-alias-filter)
+                   (error nil))
+                 (condition-case nil
+                     (advice-remove 'neovm--combo-prop-two-alias-target 'neovm--combo-prop-two-alias-filter)
+                   (error nil))
+                 (fmakunbound 'neovm--combo-prop-two-alias-target)
+                 (fmakunbound 'neovm--combo-prop-two-alias-a)
+                 (fmakunbound 'neovm--combo-prop-two-alias-b)
+                 (fmakunbound 'neovm--combo-prop-two-alias-filter)))",
+            n = n,
+        );
+
+        let (oracle, neovm) = eval_oracle_and_neovm(&form);
+        prop_assert_eq!(oracle, neovm);
+    }
+
+    #[test]
     fn oracle_prop_combination_duplicate_advice_add_remove_lifecycle_consistency(
         n in -10_000i64..10_000i64,
     ) {
