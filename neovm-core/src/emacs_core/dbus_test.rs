57292dfd807789ad946b2ff37c42e99a3b1b6c2d
Eval Exec <execvy@gmail.com>
2026-03-02 10:09:08 +0800

    Move dbus unit tests into separate file

---
+++ neovm-core/src/emacs_core/dbus_test.rs
@@ -0,0 +1,38 @@
+use super::*;
+
+#[test]
+fn dbus_init_bus_contract() {
+    assert_eq!(
+        builtin_dbus_init_bus(vec![Value::keyword(":session")]).unwrap(),
+        Value::Int(2)
+    );
+    let err = builtin_dbus_init_bus(vec![Value::Int(1)]).unwrap_err();
+    match err {
+        Flow::Signal(sig) => assert_eq!(sig.symbol_name(), "wrong-type-argument"),
+        other => panic!("expected signal, got {other:?}"),
+    }
+}
+
+#[test]
+fn dbus_get_unique_name_errors_without_connection() {
+    let err = builtin_dbus_get_unique_name(vec![Value::keyword(":system")]).unwrap_err();
+    match err {
+        Flow::Signal(sig) => assert_eq!(sig.symbol_name(), "dbus-error"),
+        other => panic!("expected signal, got {other:?}"),
+    }
+}
+
+#[test]
+fn dbus_message_internal_validates_first_arg() {
+    let err = builtin_dbus_message_internal(vec![
+        Value::keyword(":session"),
+        Value::string("/"),
+        Value::string("org.example"),
+        Value::string("Ping"),
+    ])
+    .unwrap_err();
+    match err {
+        Flow::Signal(sig) => assert_eq!(sig.symbol_name(), "wrong-type-argument"),
+        other => panic!("expected signal, got {other:?}"),
+    }
+}
